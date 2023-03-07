6d742cda65fe6405e48602c1a6fab1739fc303d0
ryannff4 <ryannff4@gmail.com>
2023-03-07 15:07:26 -0800

    added radio buttons group for selecting number of players; made it such that the generation button press properly retrieves the correct number of players even when it is changed

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React, {useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import CheckBox from '@react-native-community/checkbox';
+import RadioGroup, { RadioButtonProps } from 'react-native-radio-buttons-group';
 import {
   SafeAreaView,
   ScrollView,
@@ -31,7 +32,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
+function Section({ children, title }: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -61,12 +62,10 @@ type State = {
   hasLuxEtTenebrae: boolean;
   hasPerlaeImperii: boolean;
 };
-
-function GeneratePlacesOfPower(hasLuxEtTenebrae : boolean, hasPerlaeImperii : boolean) {
-  console.log(`Press happened.\nhasLuxEtTenebrae = ${hasLuxEtTenebrae}\nhasPerlaeImperii = ${hasPerlaeImperii}`)
-}
+var numberOfPlayers = 2
 
 function App(): JSX.Element {
+  
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -74,8 +73,47 @@ function App(): JSX.Element {
   };
 
   const [hasLuxEtTenebrae, setHasLuxEtTenebrae] = useState(false)
-  const [hasPerlaeImperii, setHasPerlaeImperii] = useState(false)
+  const [hasPerlaeImperii, setHasPerlaeImperii] = useState(false)  
 
+  function SetNumberOfPlayers(count : number) {
+    numberOfPlayers = count;
+  }
+
+  function GeneratePlacesOfPower(hasLuxEtTenebrae: boolean, hasPerlaeImperii: boolean, numberOfPlayers: number) {
+    console.log(`Press happened.\nhasLuxEtTenebrae = ${hasLuxEtTenebrae}\nhasPerlaeImperii = ${hasPerlaeImperii}\nCount of players: ${numberOfPlayers}`)
+  }
+  
+
+  const [radioButtons, setRadioButtons] = useState<RadioButtonProps[]>([
+    {
+      id: '2', // acts as primary key, should be unique and non-empty string
+      label: '2',
+      value: '2',
+      onPress: ()=>SetNumberOfPlayers(2)
+    },
+    {
+      id: '3', // acts as primary key, should be unique and non-empty string
+      label: '3',
+      value: '3',
+      onPress: ()=>SetNumberOfPlayers(3)
+    },
+    {
+      id: '4', // acts as primary key, should be unique and non-empty string
+      label: '4',
+      value: '4',
+      onPress: ()=>SetNumberOfPlayers(4)
+    },
+    {
+      id: '5', // acts as primary key, should be unique and non-empty string
+      label: '5',
+      value: '5',
+      onPress: ()=>SetNumberOfPlayers(5)
+    }
+  ]);
+
+  function onPressRadioButton(radioButtonsArray: RadioButtonProps[]) {
+    setRadioButtons(radioButtonsArray);
+  }
 
 
 
@@ -85,49 +123,49 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Expansions">   
-            <View style={{flexDirection:'column',flex:6}}>
-              <Text>{`Lux et Tenebrae: ${hasLuxEtTenebrae}`}</Text>
-              <CheckBox
-                disabled={false}
-                value={hasLuxEtTenebrae}
-                onValueChange={(value) => setHasLuxEtTenebrae(value)}
-              />
-            </View>
-            <View style={{flexDirection:'column',flex:6}}>
-              <Text>{`Perlae Imperii: ${hasPerlaeImperii}`}</Text>
-              <CheckBox
-                disabled={false}
-                value={hasPerlaeImperii}
-                onValueChange={(value) => setHasPerlaeImperii(value)}
-              />
-            </View>
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            <View>
-              <Button
-                title="Generate Randomized Places of Power"
-                onPress={() => GeneratePlacesOfPower(hasLuxEtTenebrae, hasPerlaeImperii)}
-              />
-            </View>
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Header />
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <Section title="Number of players">
+          <RadioGroup
+            radioButtons={radioButtons}
+            onPress={onPressRadioButton}
+            layout='row'
+          />
+        </Section>
+        <Section title="Expansions">
+          <View style={{ flexDirection: 'column', flex: 6 }}>
+            <Text>{`Lux et Tenebrae: ${hasLuxEtTenebrae}`}</Text>
+            <CheckBox
+              disabled={false}
+              value={hasLuxEtTenebrae}
+              onValueChange={(value) => setHasLuxEtTenebrae(value)}
+            />
+          </View>
+          <View style={{ flexDirection: 'column', flex: 6 }}>
+            <Text>{`Perlae Imperii: ${hasPerlaeImperii}`}</Text>
+            <CheckBox
+              disabled={false}
+              value={hasPerlaeImperii}
+              onValueChange={(value) => setHasPerlaeImperii(value)}
+            />
+          </View>
+        </Section>
+        <Section title="Debug">
+          <DebugInstructions />
+        </Section>
+        <Section title="Learn More">
+          <View>
+            <Button
+              title="Generate Randomized Places of Power"
+              onPress={() => GeneratePlacesOfPower(hasLuxEtTenebrae, hasPerlaeImperii, numberOfPlayers)}
+            />
+          </View>
+        </Section>
+        <LearnMoreLinks />
+      </View>
     </SafeAreaView>
   );
 }
@@ -135,7 +173,7 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 20,
-    paddingHorizontal: 24,
+    paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
